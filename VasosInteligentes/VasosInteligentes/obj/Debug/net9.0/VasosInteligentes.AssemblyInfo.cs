6bad3b9646d95d356bda2b6f82b37475efa742d0
João Pedro Pascuci De Russi <jpderussi@gmail.com>
2026-03-17 13:13:54 -0300

    Aula 17-03

--- VasosInteligentes/VasosInteligentes/obj/Debug/net9.0/VasosInteligentes.AssemblyInfo.cs
+++ VasosInteligentes/VasosInteligentes/obj/Debug/net9.0/VasosInteligentes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VasosInteligentes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31133f08d3db68440fa12c8e7c139ada3a428684")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a3999a784210dbcd0fc2265e92223db87fa4e06")]
 [assembly: System.Reflection.AssemblyProductAttribute("VasosInteligentes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VasosInteligentes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
